1405bdb3c006bae34b5262e0a89d94390e59cf8f
Joshi-hub <joshuaauerbach@web.de>
2025-11-07 19:18:31 +0100

    first commit

---
+++ models/cloud.class.js
@@ -0,0 +1,12 @@
+class Cloud extends MoveableObject {
+    y = 50;
+    height = 250;
+    width = 500;
+
+    constructor() {
+        super().loadImage('img/5_background/layers/4_clouds/1.png');
+        this.x = 50 + Math.random() * 800;
+    }
+
+
+}
