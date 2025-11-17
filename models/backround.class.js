fffc178f40e1c898cc9abf7a4d32514c801019c5
Joshi-hub <joshuaauerbach@web.de>
2025-11-17 18:13:23 +0100

    Added full JSDoc documentation for BackgroundObject

--- models/backround.class.js
+++ models/backround.class.js
@@ -1,7 +1,19 @@
+/**
+ * Represents a static background element in the game world.
+ * 
+ * BackgroundObjects are placed behind all gameplay objects
+ * and scroll with the camera, creating a parallax effect.
+ */
 class BackgroundObject extends MovableObject {
     width = 720;
     height = 480;
 
+    /**
+     * Creates a new background object and positions it in the world.
+     * 
+     * @param {string} imagePath - Path to the background image.
+     * @param {number} x - Horizontal position where the background should be placed.
+     */
     constructor(imagePath, x) {
         super().loadImage(imagePath);
         this.x = x;
